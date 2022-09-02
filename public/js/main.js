ef01930a3f71076dc061f55c4257449fbcf8011d
ChrisShep98 <shepardchristopher1@gmail.com>
2022-09-01 21:51:58 -0400

    uploading files

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+const deleteBtn = document.querySelectorAll('.del')
+const todoItem = document.querySelectorAll('span.not')
+const todoComplete = document.querySelectorAll('span.completed')
+
+Array.from(deleteBtn).forEach((el)=>{
+    el.addEventListener('click', deleteTodo)
+})
+
+Array.from(todoItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+async function deleteTodo(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// Password visability toggle
+const togglePassword = document.querySelector('.togglePassword')
+const password = document.querySelector('.password')
+
+togglePassword.addEventListener('click', function(){
+    console.log('test')
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'
+    password.setAttribute('type', type)
+
+    togglePassword.classList.toggle('bi-eye')
+})
